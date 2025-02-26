59cb96c577e84e7a55b1658f58a59b5a58855e6c
Garret Rieger <grieger@google.com>
2025-02-26 22:17:53 +0000

    Update convert_iftb to the new encoder config.

--- util/convert_iftb.h
+++ util/convert_iftb.h
@@ -3,11 +3,13 @@
 
 #include "absl/status/statusor.h"
 #include "absl/strings/string_view.h"
+#include "hb.h"
 #include "util/encoder_config.pb.h"
 
 namespace util {
 
-absl::StatusOr<EncoderConfig> convert_iftb(absl::string_view iftb_dump);
+absl::StatusOr<EncoderConfig> convert_iftb(absl::string_view iftb_dump,
+                                           hb_face_t* face);
 
 }
 
