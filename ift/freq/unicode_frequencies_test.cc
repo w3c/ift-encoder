4351bb3cd17f45a77f86283bc4027ab8cce5df9c
Garret Rieger <grieger@google.com>
2025-10-09 20:26:11 +0000

    Add unit test for UnicodeFrequencies::CoveredCodepoints().

--- ift/freq/unicode_frequencies_test.cc
+++ ift/freq/unicode_frequencies_test.cc
@@ -1,6 +1,9 @@
 #include "ift/freq/unicode_frequencies.h"
 
+#include "common/int_set.h"
 #include "gtest/gtest.h"
+
+using common::CodepointSet;
 
 namespace ift::freq {
 
@@ -53,6 +56,19 @@ TEST(UnicodeFrequenciesTest, AddAccumulates) {
   freq.Add(2, 1, 15);
   EXPECT_DOUBLE_EQ(freq.ProbabilityFor(1, 2), 1.0);
   EXPECT_DOUBLE_EQ(freq.ProbabilityFor(2, 3), 20.0 / 25.0);
+}
+
+TEST(UnicodeFrequenciesTest, CoveredCodepoints) {
+  UnicodeFrequencies freq{
+      {{1, 2}, 10},
+      {{1, 1}, 5},
+      {{3, 2}, 20},
+      {{5, 5}, 10},
+  };
+  EXPECT_EQ(freq.CoveredCodepoints(), (CodepointSet{1, 5}));
+
+  UnicodeFrequencies empty;
+  EXPECT_EQ(empty.CoveredCodepoints(), (CodepointSet{}));
 }
 
 }  // namespace ift::freq
